88e16ab15a2d2393945e6bb6817152abb6a6ac17
GlabPiotr <gb.piotr@gmail.com>
2018-06-12 22:01:50 +0200

    Frontend side.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,4 @@
-// import {StudentsService} from "./services/students.service";
+import {StudentsService} from "./services/students.service";
 import {Component} from "@angular/core";
 import {Student} from "./models/students";
 import {MatTableDataSource} from "@angular/material";
@@ -17,37 +17,47 @@ export class AppComponent {
   id: number;
   students: Array<any> = [new Student("a", "b", 1), new Student("c", "d", 2)];
 
-  columnsNames = ['select', 'Name', 'Last name', 'Id'];
+  columnsNames = ['Select', 'Name', 'Last Name', 'Identity Number'];
 
   dataSource = new MatTableDataSource<Element>(this.students);
-  selection = new SelectionModel<any>(true, []);
+  selection = new SelectionModel<any>(false, []);
 
+  isActionsButtonDisabled = true;
+
+  selectedStudent: Student = null;
 
   constructor() {
   }
 
-  // private _onCreateButtonClick(): void {
-  //   this.studentsService.createStudent(new Student(this.name, this.lastName, this.identityNumber)).subscribe(() => {
-  //   });
-  // }
-  //
-  // private _onGetStudentByIdButtonClick(): void {
-  //   this.studentsService.getStudentById(this.id).subscribe(elem => this.students = [new Student("y", "w", 7)]);
-  // }
-  //
-  // private _onDeleteButtonClick(): void {
-  //   this.studentsService.deleteStudent(this.id).subscribe(() => {
-  //   });
-  // }
-  //
-  // private _onModifyButtonClick(): void {
-  //   this.studentsService.modifyStudent(new Student(this.name, this.lastName, this.identityNumber), this.id).subscribe(() => {
-  //   });
-  // }
+  _onItemClicked(event, selection, data): void {
+    event.stopPropagation();
+    this.selectedStudent = data;
+    this._enableActionButtons();
+  };
+
+
+  _enableActionButtons(): void {
+    this.isActionsButtonDisabled = false;
+  }
+
+  private _onCreateButtonClick(): void {
+    this.studentsService.createStudent(new Student(this.name, this.lastName, this.identityNumber)).subscribe(() => {
+    });
+  }
+
+  private _onGetStudentByIdButtonClick(): void {
+    this.studentsService.getStudentById(this.id).subscribe(elem => this.students = [new Student("y", "w", 7)]);
+  }
+
+  private _onDeleteButtonClick(): void {
+    this.studentsService.deleteStudent(this.selectedStudent.id).subscribe(() => {
+    });
+  }
+
 
   private _onGetStudents(): void {
     this.students = [new Student("a", "b", 1), new Student("c", "d", 2)];
-    // this.studentsService.getStudents().subscribe(elem => this.students = [new Student("a", "b", 1), new Student("c", "d", 2)]);
+    this.studentsService.getStudents().subscribe(elem => this.students = [new Student("a", "b", 1), new Student("c", "d", 2)]);
   }
 
 }
